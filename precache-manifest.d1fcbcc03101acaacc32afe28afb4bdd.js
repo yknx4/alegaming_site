f4f494843bbe9253285c248a3fa68ace8cb602c8
Ale Ornelas <me@ale.world>
2020-09-26 16:16:09 -0500

    Updates

---
+++ precache-manifest.d1fcbcc03101acaacc32afe28afb4bdd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ea3199419bb5e37499c896c2842a702",
+    "url": "/alegaming_site/index.html"
+  },
+  {
+    "revision": "c5e5ff96f51f33ba9394",
+    "url": "/alegaming_site/static/css/2.6df4c8f8.chunk.css"
+  },
+  {
+    "revision": "aec57c3b34404470b48c",
+    "url": "/alegaming_site/static/css/main.942da884.chunk.css"
+  },
+  {
+    "revision": "c5e5ff96f51f33ba9394",
+    "url": "/alegaming_site/static/js/2.01f7fb22.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/alegaming_site/static/js/2.01f7fb22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aec57c3b34404470b48c",
+    "url": "/alegaming_site/static/js/main.e2d2bb61.chunk.js"
+  },
+  {
+    "revision": "0f1c8878bbb62cdc35f3",
+    "url": "/alegaming_site/static/js/runtime-main.ab519dba.js"
+  }
+]);
