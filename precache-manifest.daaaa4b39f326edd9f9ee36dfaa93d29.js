987ef9d77f40ead89034e62fc2108bf50e71b534
Jade Ornelas Figueroa <afigueroa@goamv.com>
2020-10-03 14:42:11 -0500

    Updates

---
+++ precache-manifest.daaaa4b39f326edd9f9ee36dfaa93d29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1457ceac79b6d469927a8fd58862f00",
+    "url": "/alegaming_site/index.html"
+  },
+  {
+    "revision": "323d6057eb9990abab19",
+    "url": "/alegaming_site/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "5a5edb61d47ff4a4a832",
+    "url": "/alegaming_site/static/css/main.7783d4c0.chunk.css"
+  },
+  {
+    "revision": "323d6057eb9990abab19",
+    "url": "/alegaming_site/static/js/2.d2336b10.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/alegaming_site/static/js/2.d2336b10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a5edb61d47ff4a4a832",
+    "url": "/alegaming_site/static/js/main.06f84a27.chunk.js"
+  },
+  {
+    "revision": "0f1c8878bbb62cdc35f3",
+    "url": "/alegaming_site/static/js/runtime-main.ab519dba.js"
+  }
+]);
